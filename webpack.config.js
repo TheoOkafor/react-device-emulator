e92552c9b39951bc4910a248d6e1b3e286f34140
TheoOkafor <theo.okafor@yahoo.com>
2023-01-10 14:32:48 +0100

    chore: npm vulnerability fix and dependency upgrades

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,55 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /(\.s[ac]ss$|\.css$)/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(jpe?g|gif|png|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-        loader: 'file-loader?name=images/[name].[ext]',
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          plugins: ['@babel/plugin-proposal-class-properties'],
-        }
-      },
-      {
-        test: /\.(js|jsx)$/,
-        use: ['eslint-loader']
-      }
-    ],
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.css']
+    open: true,
+    host: "localhost",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "src/index.html",
     }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
 };
